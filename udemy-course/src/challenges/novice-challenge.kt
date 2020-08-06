11cd16ef4e22e93af3b5683d619fccd342eef10c
Payel Senapati <payelsenapati.nayan@gmail.com>
2020-08-06 13:24:55 +0530

    Update novice-challenge.kt

--- udemy-course/src/challenges/novice-challenge.kt
+++ udemy-course/src/challenges/novice-challenge.kt
@@ -1,21 +1,24 @@
-package challenges
+fun main() {
+    val myBookArrayList: ArrayList<String> = arrayListOf(
+            "In Search of Lost Time",
+            "Ulysses",
+            "Don Quixote",
+            "The Great Gatsby",
+            "One Hundred Years of Solitude",
+            "Moby Dick",
+            "War and Peace",
+            "Lolita",
+            "Hamlet",
+            "The Catcher in the Rye"
+    )
 
-/**
- * @author Peter Sommerhoff
- *
- * Solution for the novice challenge inside the Udemy course.
- * Creates a list of book titles, loops over it and, if the title contains 'e', prints each char of the title in a line.
- */
-fun main(args: Array<String>) {
-
-    val books = arrayListOf("Kotlin in Action", "Kotlin For Android Developers", "The Phoenix Project")
-
-    for (book in books) {
+    for (book in myBookArrayList) {
         if (book.contains('e')) {
-            for (character in book) {
-                println(character)
+            for (myChar in book) {
+                println(myChar)
             }
+            println("#######################################################################")
         }
-        println()
+
     }
 }
